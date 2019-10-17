65d0dfd122fc81bfd59e895bb141de3f79024f39
skadmsfl <noeytt.pk@gmail.com>
2019-10-17 22:13:48 +0700

    YouTubeBaseActivity

--- app/src/main/java/com/example/test7/YouTubeActivity.kt
+++ app/src/main/java/com/example/test7/YouTubeActivity.kt
@@ -2,11 +2,22 @@ package com.example.test7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 
-class YouTubeActivity : AppCompatActivity() {
+class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_you_tube)
+        setContentView(R.layout.activity_youtube)
+    }
+
+    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
